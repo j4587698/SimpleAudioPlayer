36be71698d6e984ed4b64bfdeca9222759e39ebc
jx <jx@jvxiang.com>
2025-03-10 14:23:54 +0800

    添加自定义Handle

---
+++ SimpleAudioPlayer/Handles/CustomHandle.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using SimpleAudioPlayer.Enums;
+
+namespace SimpleAudioPlayer.Handles;
+
+public class CustomHandle(
+    Func<byte[], int, int, int> onRead,
+    Func<long, SeekOrigin, bool> onSeek,
+    Func<long> onTell): AudioCallbackHandlerBase
+{
+    public override void Dispose()
+    {
+    }
+
+    public override MaResult OnRead(IntPtr pDecoder, IntPtr pBuffer, ulong bytesToRead, out UIntPtr bytesRead)
+    {
+        var buffer = new byte[bytesToRead];
+        var read = onRead(buffer, 0, (int)bytesToRead);
+        Marshal.Copy(buffer, 0, pBuffer, read);
+        bytesRead = (UIntPtr)read;
+        
+        return read > 0 ? MaResult.MaSuccess : MaResult.MaAtEnd;
+    }
+
+    public override MaResult OnSeek(IntPtr pDecoder, long offset, SeekOrigin origin)
+    {
+        return onSeek(offset, origin) ? MaResult.MaSuccess : MaResult.MaNotImplemented;
+    }
+
+    public override MaResult OnTell(IntPtr pDecoder, out UIntPtr pCursor)
+    {
+        var cursor = onTell();
+        pCursor = (UIntPtr)cursor;
+        return MaResult.MaSuccess;
+    }
+}
