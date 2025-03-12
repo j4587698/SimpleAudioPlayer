6d81fae8116a81561a6785b054b3fa38532a95f5
jx <jx@jvxiang.com>
2025-03-12 17:14:39 +0800

    增加device相关处理

---
+++ SimpleAudioPlayer/Native/DeviceCallbacks.cs
@@ -0,0 +1,70 @@
+﻿using System.Runtime.InteropServices;
+using SimpleAudioPlayer.Enums;
+using SimpleAudioPlayer.Utils;
+
+namespace SimpleAudioPlayer.Native;
+
+public class DeviceCallbacks: IDisposable
+{
+    private readonly ContextAwareScheduler _scheduler;
+    private readonly object _syncRoot = new();
+    private readonly AudioContextHandle _ctx;
+    
+    private GCHandle _onStopHandle;
+    private GCHandle _deviceStateChangedCallback;
+    private NativeMethods.StopCallback StopProxy { get; }
+    private NativeMethods.DeviceStateChangedCallback DeviceStateChangedProxy { get; }
+    
+    public Action<MaDeviceNotificationType>? DeviceStateChanged { get; set; }
+
+    public DeviceCallbacks(AudioContextHandle ctx, SampleFormat sampleFormat = SampleFormat.F32, uint channels = 2, uint sampleRate = 44100)
+    {
+        _ctx = ctx;
+        _scheduler = new ContextAwareScheduler();
+        StopProxy = ProxyStop;
+        DeviceStateChangedProxy = ProxyDeviceStateChanged;
+        
+        _onStopHandle = GCHandle.Alloc(StopProxy);
+        _deviceStateChangedCallback = GCHandle.Alloc(DeviceStateChangedProxy);
+        NativeMethods.AudioInitDevice(_ctx, StopProxy, DeviceStateChangedProxy, sampleFormat, channels, sampleRate);
+    }
+    
+    
+    private void ProxyStop()
+    {
+        _scheduler.Post(() =>
+        {
+            lock (_syncRoot)
+            {
+                NativeMethods.AudioStop(_ctx);
+            }
+        });
+    }
+
+    private void ProxyDeviceStateChanged(IntPtr pNotification)
+    {
+        var notificationType = GetNotificationType(pNotification);
+        DeviceStateChanged?.Invoke(notificationType);
+    }
+    
+    private static MaDeviceNotificationType GetNotificationType(IntPtr pNotification)
+    {
+        return (MaDeviceNotificationType)Marshal.ReadInt32(
+            pNotification, 
+            IntPtr.Size // 自动适应 x86/x64
+        );
+    }
+
+    public void Dispose()
+    {
+        if (_onStopHandle.IsAllocated)
+        {
+            _onStopHandle.Free();
+        }
+
+        if (_deviceStateChangedCallback.IsAllocated)
+        {
+            _deviceStateChangedCallback.Free();
+        }
+    }
+}
---
+++ SimpleAudioPlayer/Utils/ContextAwareScheduler.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleAudioPlayer.Utils;
+
+public class ContextAwareScheduler(
+    SynchronizationContext? syncContext = null,
+    TaskScheduler? scheduler = null)
+{
+    private readonly SynchronizationContext? _syncContext = syncContext ?? SynchronizationContext.Current;
+    private readonly TaskScheduler _taskScheduler = scheduler ?? TaskScheduler.Default;
+
+    public void Post(Action action)
+    {
+        if (_syncContext != null)
+        {
+            _syncContext.Post(_ => action(), null);
+        }
+        else
+        {
+            Task.Factory.StartNew(action, 
+                CancellationToken.None, 
+                TaskCreationOptions.DenyChildAttach, 
+                _taskScheduler);
+        }
+    }
+}
